0d5f3937b4c51f408c0e2009406f01b9ecd85436
jonathan bola <bolajonathan@yahoo.com>
2022-06-04 08:57:01 -0500

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import AddMovie from './AddMovie';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>        
+        <Route exact path="/addMovie">
+          <AddMovie />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
