3ab68e320f464e827adf7359dfba1dbaa3b4e91e
jonathan bola <bolajonathan@yahoo.com>
2022-06-04 08:47:25 -0500

    get requests for movies

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/movies" do
+    movies= Movie.all 
+    movies.to_json
+    
   end
+  
+  get "/movies/:title" do
+    # binding.pry
+    movies = Movie.where("title LIKE ?", params[:title] + "%")
+    movies.to_json
+  end
+
+
 
 end
