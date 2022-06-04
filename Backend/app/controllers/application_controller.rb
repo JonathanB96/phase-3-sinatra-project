c548397f56aa3bf5b17acf824098b19c9ffc8650
jonathan bola <bolajonathan@yahoo.com>
2022-06-04 08:48:28 -0500

    movies post request

--- Backend/app/controllers/application_controller.rb
+++ Backend/app/controllers/application_controller.rb
@@ -8,11 +8,23 @@ class ApplicationController < Sinatra::Base
     movies.to_json
     
   end
-  
+
   get "/movies/:title" do
     # binding.pry
     movies = Movie.where("title LIKE ?", params[:title] + "%")
     movies.to_json
+  end
+
+  post '/movies' do
+    # binding.pry    
+    new_movie = Movie.create(
+      title:params[:title], 
+      year:params[:year], 
+      image_url:params[:image_url],
+      genre_id: Genre.find_by(name: params[:genre]).id)
+      
+    new_movie.to_json
+    
   end
 
 
